4b7bd625ddc45fdde048e74f853871e61df54265
tehw0lf <tehwolf@protonmail.com>
2018-01-10 21:20:54 +0100

    Add setup.py
    A setup file was added to allow installation via pip. Also updated travis.yml to reflect the changes.
    
    Fixes #4

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='paw',
+      version='1.1',
+      description='paw - patterns and wordlists in python',
+      url='https://github.com/tehw0lf/paw',
+      author='tehw0lf',
+      author_email='tehwolf@protonmail.com',
+      license='MIT',
+      packages=['paw'],
+      zip_safe=False)
