27890cc12bbe35ac54bc14035da8410832dd7d66
tehw0lf <robert.weyres@ruhr-uni-bochum.de>
2018-02-21 23:48:29 +0100

    Remove functions from class
    The generator was removed from the class, and the tests were adjusted to reflect the changes.
    
    Part of #14

--- paw/tests/test_gen_wordlist.py
+++ paw/tests/test_gen_wordlist.py
@@ -5,11 +5,11 @@ class gen_wordlist_test(paw_test):
     def test_gen_wordlist(self):
         self.paw.gensets = '[ABC][123]'
         self.paw.parse_cset()
-        words1 = list(self.paw.gen_wordlist())
+        words1 = list(self.gen_wordlist(self.paw.cset))
         self.paw.cset = {}
         self.paw.gensets = '[A][123]'
         self.paw.parse_cset()
-        words2 = list(self.paw.gen_wordlist())
+        words2 = list(self.gen_wordlist(self.paw.cset))
         self.assertEqual(words1, ['A1', 'A2', 'A3', 'B1', 'B2',
                                   'B3', 'C1', 'C2', 'C3'], "test successful")
         self.assertEqual(words2, ["A1", "A2", "A3"])
@@ -17,11 +17,11 @@ class gen_wordlist_test(paw_test):
     def test_gen_wordlist_no_duplicates(self):
         self.paw.gensets = '[AA][11]'
         self.paw.parse_cset()
-        no_duplicates = list(self.paw.gen_wordlist())
+        no_duplicates = list(self.gen_wordlist(self.paw.cset))
         self.assertEqual(no_duplicates, ["A1"])
 
     def test_gen_wordlist_from_file_no_duplicates(self):
         self.paw.infile = "paw/tests/test_files/gen_words_sample.txt"
         self.paw.gen_custom_charset()
-        words3 = list(self.paw.gen_wordlist())
+        words3 = list(self.gen_wordlist(self.paw.cset))
         self.assertEqual(words3, ["B1"])
