dae9a2e4906c5a865eea44c04ebee8fd436a5951
tehw0lf <tehwolf@protonmail.com>
2018-04-10 23:06:58 +0200

    Introduce buffering
    After implementing a buffer list, a major speedup could be observed when using a buffer size of 256.
    
    Resolves #14, part of #9

--- paw/command_line.py
+++ paw/command_line.py
@@ -27,6 +27,12 @@ def parse_cmdline():
             generate pattern and print it
             ''')
         parser.add_argument(
+            '-b',
+            action='store',
+            dest='max_buf',
+            default=256,
+            help=' Maximum buffer size (default: 256)')
+        parser.add_argument(
             '-g',
             action='store',
             dest='gensets',
@@ -79,7 +85,7 @@ def main():
                 exit()
             else:
                 paw_cli.gen_custom_charset()
-                paw_cli.save_wordlist(args.outfile)
+                paw_cli.save_wordlist(args.outfile, args.max_buf)
                 if args.hcat:
                     paw_cli.gen_hcat_cmd()
         else:
@@ -94,7 +100,7 @@ def main():
             exit()
         else:
             paw_cli.parse_cset()
-            paw_cli.save_wordlist(args.outfile)
+            paw_cli.save_wordlist(args.outfile, args.max_buf)
 
     if args.pattern:
         if args.infile:
