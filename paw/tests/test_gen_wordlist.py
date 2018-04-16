515fe008f59b111d878b8fefc978f571e4c1eeaf
tehw0lf <tehwolf@protonmail.com>
2018-04-17 01:40:09 +0200

    Update unit tests
    The unit tests were updated to test all algorithms that are imported from wlgen.

--- paw/tests/test_gen_wordlist.py
+++ paw/tests/test_gen_wordlist.py
@@ -1,3 +1,4 @@
+import wlgen
 from .base import paw_test
 
 
@@ -14,14 +15,29 @@ class gen_wordlist_test(paw_test):
                                   'B3', 'C1', 'C2', 'C3'], "test successful")
         self.assertEqual(words2, ["A1", "A2", "A3"])
 
-    def test_gen_wordlist_no_duplicates(self):
-        self.paw.gensets = '[AA][11]'
-        self.paw.parse_cset()
-        no_duplicates = list(self.gen_wordlist(self.paw.cset))
-        self.assertEqual(no_duplicates, ["A1"])
-
     def test_gen_wordlist_from_file_no_duplicates(self):
         self.paw.infile = "paw/tests/test_files/gen_words_sample.txt"
         self.paw.gen_custom_charset()
         words3 = list(self.gen_wordlist(self.paw.cset))
         self.assertEqual(words3, ["B1"])
+
+    def test_gen_wordlist_iter_no_duplicates(self):
+        self.paw.gen_wordlist = wlgen.gen_wordlist_iter
+        self.paw.gensets = '[AA][11]'
+        self.paw.parse_cset()
+        no_duplicates = list(self.gen_wordlist(self.paw.cset))
+        self.assertEqual(no_duplicates, ["A1"])
+
+    def test_gen_wordlist_no_duplicates(self):
+        self.paw.gen_wordlist = wlgen.gen_wordlist
+        self.paw.gensets = '[AA][11]'
+        self.paw.parse_cset()
+        no_duplicates = list(self.gen_wordlist(self.paw.cset))
+        self.assertEqual(no_duplicates, ["A1"])
+
+    def test_gen_words_no_duplicates(self):
+        self.paw.gen_wordlist = wlgen.gen_words
+        self.paw.gensets = '[AA][11]'
+        self.paw.parse_cset()
+        no_duplicates = list(self.gen_wordlist(self.paw.cset))
+        self.assertEqual(no_duplicates, ["A1"])
