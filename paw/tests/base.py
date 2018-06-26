74e4a55e740cc65a333d01f57c875aa735d8863d
tehw0lf <tehwolf@protonmail.com>
2018-06-26 21:26:14 +0200

    Reformat project with black
    In order to facilitate future work, black is now being used to format the code automatically.

--- paw/tests/base.py
+++ paw/tests/base.py
@@ -9,9 +9,9 @@ class paw_test(unittest.TestCase):
         self.paw = paw.Paw()
         self.w1 = [["A", "B", "C"], ["1", "2", "3"]]
         self.w2 = [["A"], ["1", "2", "3"]]
-        self.w3 = 'a'
+        self.w3 = "a"
         self.no_duplicates = [["A", "A"], ["1", "1"]]
         self.badchar = chr(0)
-        self.tfilepath = 'paw/tests/test_files/tmp'
-        self.tfile = open(self.tfilepath, 'w')
+        self.tfilepath = "paw/tests/test_files/tmp"
+        self.tfile = open(self.tfilepath, "w")
         self.tfile.close()
