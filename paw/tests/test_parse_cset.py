c7ca457b43e7cccdfb1651e9788feae8343d396a
tehw0lf <tehwolf@protonmail.com>
2018-07-14 19:10:06 +0200

    Add empty charset test
    The sys.exit() call is now being tested.

--- paw/tests/test_parse_cset.py
+++ paw/tests/test_parse_cset.py
@@ -8,6 +8,11 @@ class parse_cset_test(paw_test):
         (key, value), = self.paw.cset.items()
         self.assertEqual(value, "0123456789d")
 
+    def test_parse_cset_empty(self):
+        self.paw.gensets = "[]"
+        with self.assertRaises(SystemExit):
+            self.paw.parse_cset()
+
     def test_parse_cset_lbracket(self):
         self.paw.gensets = "[[]"
         self.paw.parse_cset()
