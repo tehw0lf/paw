19ddb4e8949bf7ed85ada515a29fe87edf972177
tehw0lf <tehwolf@protonmail.com>
2018-07-14 18:44:32 +0200

    Update parse_cset() tests
    The test for uneven brackets is no longer needed, instead there are two new tests to assure that single brackets are accepted within character sets.

--- paw/tests/test_parse_cset.py
+++ paw/tests/test_parse_cset.py
@@ -8,7 +8,14 @@ class parse_cset_test(paw_test):
         (key, value), = self.paw.cset.items()
         self.assertEqual(value, "0123456789d")
 
-    def test_parse_cset_uneven(self):
-        self.paw.gensets = "[a"
+    def test_parse_cset_lbracket(self):
+        self.paw.gensets = "[[]"
         self.paw.parse_cset()
-        self.assertEqual(self.paw.wcount, 1)
+        (key, value), = self.paw.cset.items()
+        self.assertEqual(value, "[")
+
+    def test_parse_cset_rbracket(self):
+        self.paw.gensets = "[]]"
+        self.paw.parse_cset()
+        (key, value), = self.paw.cset.items()
+        self.assertEqual(value, "]")
