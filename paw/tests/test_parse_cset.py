684f068b512fcf6e8cf5ca2b60ed7b07232432a0
tehw0lf <tehwolf@protonmail.com>
2018-09-14 23:26:03 +0200

    Improve code quality
    Now featuring enumerate and no more dead code.

--- paw/tests/test_parse_cset.py
+++ paw/tests/test_parse_cset.py
@@ -5,8 +5,9 @@ class parse_cset_test(paw_test):
     def test_parse_cset(self):
         self.paw.gensets = "[%dd]"
         self.paw.parse_cset()
-        (key, value), = self.paw.cset.items()
-        self.assertEqual(value, "0123456789d")
+        values = "".join(self.paw.cset.values())
+        print(values)
+        self.assertEqual(values, "0123456789d")
 
     def test_parse_cset_empty(self):
         self.paw.gensets = "[]"
@@ -16,11 +17,11 @@ class parse_cset_test(paw_test):
     def test_parse_cset_lbracket(self):
         self.paw.gensets = "[[]"
         self.paw.parse_cset()
-        (key, value), = self.paw.cset.items()
-        self.assertEqual(value, "[")
+        values = "".join(self.paw.cset.values())
+        self.assertEqual(values, "[")
 
     def test_parse_cset_rbracket(self):
         self.paw.gensets = "[]]"
         self.paw.parse_cset()
-        (key, value), = self.paw.cset.items()
-        self.assertEqual(value, "]")
+        values = "".join(self.paw.cset.values())
+        self.assertEqual(values, "]")
